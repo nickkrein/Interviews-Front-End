4ea82e505cfa3b537f30054541d8cfc5cd82b4dd
Nick Krein <nkreinmusic@gmail.com>
2016-07-22 19:40:26 -0700

    fetch json data from endpoint and pass to child

--- react-petlist/src/components/SitterSearch.js
+++ react-petlist/src/components/SitterSearch.js
@@ -2,12 +2,35 @@ import React, { Component } from 'react';
 import SitterList from './SitterList';
 
 class SitterSearch extends Component {
+  
+  constructor(props) {
+    super(props)
+    this.state = {data : []};
+  }
+
+  loadSitters() {
+  	let location = window.location.origin; // http://localhost:3000
+
+  	fetch(location + '/static/search.json', {method: 'get'}).then((response) => {
+  		//convert response to json
+  		return response.json();
+  	}).then((j) => {
+  		console.log(j.search);
+  		this.setState({data : j.search})
+  	}).catch(function(err) {
+  		console.log (err);
+  	})
+  }
+
+  componentDidMount() {
+  	this.loadSitters();
+  }
 
   render() {
     return (
     <div className='sitterSearch'>
     	<h1>DogVacay</h1>
-    	<SitterList />
+    	<SitterList data={this.state.data} />
     </div>
     );
   }
