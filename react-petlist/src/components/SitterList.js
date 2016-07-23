eedcd1cec72262b3720c061558a66d7b4d7d9ec2
Nick Krein <nkreinmusic@gmail.com>
2016-07-23 00:35:41 -0700

    implement SitterList, map over data to create Sitter nodes

--- react-petlist/src/components/SitterList.js
+++ react-petlist/src/components/SitterList.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 import Sitter from './Sitter'
 
 class SitterList extends React.Component {
-  
+  createUrl() {
+
+  }
+
   render() {
+    let sitterNodes = this.props.data.map(function(sitter){
+      // assuming sitter.pet.id is unique
+      return (
+        <Sitter title={sitter.title} decription={sitter.description} user={sitter.user} pet={sitter.pet} url={sitter.url} description={sitter.description} key={sitter.pet.id} />
+      );
+    });
     return (
       <div>
-        <p>SitterList Placeholder</p>
-        <Sitter />
+        {sitterNodes}
       </div>
     );
   } 
