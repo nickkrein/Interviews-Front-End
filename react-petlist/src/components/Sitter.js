48d8af51d8d493af3be6279123c5c01c90310ae3
Nick Krein <nkreinmusic@gmail.com>
2016-07-23 00:34:38 -0700

    implement Sitter with data from API for href and content

--- react-petlist/src/components/Sitter.js
+++ react-petlist/src/components/Sitter.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
 class Sitter extends Component {
-	
+
 	render() {
 		return (
-			<div>Sitter Placeholder</div>
+			<a href={this.props.url} className="sitter">
+				<div> {this.props.title} </div>
+				<div> {this.props.user.first} {this.props.user.last} </div>
+				<div> {this.props.pet.name} the {this.props.pet.type} </div>
+				<div> {this.props.description} </div>
+			</a>
 		);
 	}
 };
